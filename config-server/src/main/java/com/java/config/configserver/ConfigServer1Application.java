7b8620d2c521120c26bacdb271dbde8720ab84d3
ratanchand10 <ratanchandya10@gmail.com>
2019-06-13 09:36:03 +0530

    FirstCommit Config-service

---
+++ config-server/src/main/java/com/java/config/configserver/ConfigServer1Application.java
@@ -0,0 +1,15 @@
+package com.java.config.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServer1Application.class, args);
+	}
+
+}
